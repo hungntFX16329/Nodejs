271ec6ba071d68dcbeedd60cf7dcd6e12551689d
Tien Hung <hungntFX16329@funix.edu.vn>
2022-06-22 08:05:49 +0700

    Lab 2.4

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const http = require('http');
 const express = require('express')
 
 const app = express();
-app.use((req,res,next)=>{
-    console.log('In the middleware !');
+
+app.use('/',(req,res,next)=>{
+    console.log('This is always run !');
     next();
 })
 
-app.use((req,res,next)=>{
+app.use('/add-product',(req,res,next)=>{
+    console.log('In another the middleware !');
+    res.send('<h1>The "Add product" Page </h1>')
+})
+
+app.use('/',(req,res,next)=>{
     console.log('In another the middleware !');
     res.send('<h1>Hello from Express !</h1>')
 })
