840a038d3f9dccaad2c2448e9054149ea889a0a3
Tien Hung <hungntFX16329@funix.edu.vn>
2022-06-22 09:20:31 +0700

    Lab 2.5

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const http = require('http');
 
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use('/',(req,res,next)=>{
-    console.log('This is always run !');
-    next();
-})
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/add-product',(req,res,next)=>{
-    console.log('In another the middleware !');
-    res.send('<h1>The "Add product" Page </h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>')
+})
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
 })
 
 app.use('/',(req,res,next)=>{
-    console.log('In another the middleware !');
     res.send('<h1>Hello from Express !</h1>')
 })
 
