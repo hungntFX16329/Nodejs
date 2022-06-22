df319c03687fb800ca5962cff537828f3c5937c7
Tien Hung <hungntFX16329@funix.edu.vn>
2022-06-22 14:48:26 +0700

    Lab 2.14

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const http = require('http');
 const path = require('path');
-const rootDir = require('./util/path');
+
 
 const express = require('express')
 const bodyParser = require('body-parser')
+
 const app = express();
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname,'public')))
+
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
