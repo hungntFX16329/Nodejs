ea80aa8ba4b6228db0085812c0920a914ef7d863
Tien Hung <hungntFX16329@funix.edu.vn>
2022-06-23 16:15:39 +0700

    Lab 3.1

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const rootDir = require('../util/path');
 const express = require('express');
-const router = express.Router();
+const adminData = require('./admin')
 
+const router = express.Router();
 router.get('/',(req,res,next)=>{
+    console.log('shop.js',adminData.products);
     res.sendFile(path.join(rootDir,'views','shop.html'))
 })
 
