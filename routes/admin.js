88f4d87d937e8313cd5910631996175e75ad05df
Tien Hung <hungntFX16329@funix.edu.vn>
2022-06-26 20:37:56 +0700

    Lab 4.1

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const path = require('path');
-const rootDir = require('../util/path');
+
+const productsController = require('../controllers/products')
+
 const router = express.Router();
-const products = []
 
-router.get('/add-product',(req,res,next)=>{
-    res.render('add-product',{
-        pageTitle: 'Add product', 
-        path:'/admin/add-product',
-        formsCss : true,
-        productCss: true,
-        activeAddProduct: true
-    });
-})
 
-router.post('/add-product',(req,res,next)=>{
-    products.push({title : req.body.title });
-    res.redirect('/')
-})
+router.get('/add-product',productsController.getAddProduct)
 
-exports.routes = router;
-exports.products = products;
+router.post('/add-product',productsController.postAddProduct)
+
+module.exports = router;
