4b482e0fb874cd1b449d4bbed06f4bcbfefd2a94
Tien Hung <hungntFX16329@funix.edu.vn>
2022-06-27 08:00:50 +0700

    Lab 4.6

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const http = require('http');
 const path = require('path');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views','views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin')
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-const errorController = require('./controllers/error')
-app.use(errorController.get404)
+app.use(errorController.get404);
 
-const server = http.createServer(app)
-server.listen(3000);
+app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
+const productsController = require('../controllers/products');
+
 const router = express.Router();
-const productsController = require('../controllers/products')
-router.get('/',productsController.getProducts)
+
+router.get('/', productsController.getProducts);
 
 module.exports = router;
