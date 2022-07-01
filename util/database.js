61b3406529ad9d387e40b602182be442a571a9ba
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-01 09:59:31 +0700

    Lab 5.8

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '19H31733a@!'
-})
+const sequelize = new Sequelize('node-complete','root','19H31733a@!',{
+    dialect:'mysql',
+    host:'localhost'
+});
 
-module.exports = pool.promise();
+module.exports = sequelize;
