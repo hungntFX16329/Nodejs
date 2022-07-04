4d82eaaf9d2570603d4ec7b377ea0a1aa6a63ccf
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-04 21:14:25 +0700

    Lab 6.18

--- models/user.js
+++ models/user.js
@@ -15,7 +15,20 @@ class User {
     return db.collection('users').insertOne(this)
   }
   addToCart(product){
-    const updatedCart = {items: [{productId: new ObjectId(product._id),quantity: 1}]};
+    const cartProductIndex = this.cart.items.findIndex(cp=>{
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items]
+    if(cartProductIndex >=0){
+      newQuantity = this.cart.items[cartProductIndex].quantity +1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    }else{
+      updatedCartItems.push({productId: new ObjectId(product._id),quantity: newQuantity})
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    };
     const db = getDb();
     return db
     .collection('users')
