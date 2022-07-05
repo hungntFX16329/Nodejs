1fc037a93c511ae0b5bf53a6a420f30c7bbd8364
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-05 08:25:27 +0700

    Lab 6.24

--- models/user.js
+++ models/user.js
@@ -101,9 +101,12 @@ class User {
     })
   }
 
-  getOrder(){
+  getOrders(){
     const db = getDb();
-    return db.collection('orders')
+    return db
+    .collection('orders')
+    .find({'user._id': new ObjectId(this._id)})
+    .toArray()
   }
 
   static findById(userId) {
