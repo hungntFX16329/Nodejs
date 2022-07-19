a3c1c6aacf240b0457c958a920d690dc8ad959e1
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-19 08:47:03 +0700

    Lab 9.1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
+const {validationResult} = require('express-validator/check');
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -64,6 +65,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()
+    })
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
