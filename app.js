f709ec8ecdbdb807f350f730bf4a293bf32a6996
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-20 14:54:23 +0700

    Lab 10.5

--- app.js
+++ app.js
@@ -29,6 +29,17 @@ const fileStore = multer.diskStorage({
     cb(null, new Date().toISOString() +'-'+ file.originalname)
   }
 });
+const fileFilter = (req,file,cb) =>{
+  if(
+    file.mimetype === 'image/png' || 
+    file.mimetype === 'image/jpg' || 
+    file.mimetype === 'image/jpeg'
+  ){
+    cb(null,true)
+  } else {
+    cb(null,false)
+  }
+}
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -38,7 +49,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStore}).single('image'))
+app.use(multer({ storage: fileStore, fileFilter: fileFilter}).single('image'))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
