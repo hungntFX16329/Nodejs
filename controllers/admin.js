9b065973b09fce0e0bff122e09986b064d4450dd
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-20 14:13:23 +0700

    Lab 10.2

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,8 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const imageUrl = req.body.image;
+  console.log(imageUrl);
   const price = req.body.price;
   const description = req.body.description;
   const errors = validationResult(req);
