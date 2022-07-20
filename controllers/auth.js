749d3d73298aebeb02abf441bc8dd0d5b3677487
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-20 13:52:45 +0700

    Lab 10.1

--- controllers/auth.js
+++ controllers/auth.js
@@ -99,7 +99,11 @@ exports.postLogin = (req, res, next) => {
         res.redirect('/login')
       })
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -134,7 +138,9 @@ exports.postSignup = (req, res, next) => {
       res.redirect('/login');
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
