2f33776d8203d96faf4afebec8e098e1b690b4a2
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-20 15:15:40 +0700

    Lab 10.6

--- routes/admin.js
+++ routes/admin.js
@@ -35,8 +35,6 @@ router.post('/edit-product', [
         .isString()
         .isLength({min:3})
         .trim(),
-    body('imageUrl')
-        .isURL(),
     body('price')
         .isFloat(),
     body('description')
