f1565bc55ec6ea6c76ba4e1c4fbd45d2250c2fde
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-21 08:59:20 +0700

    Lab 10.9

--- routes/shop.js
+++ routes/shop.js
@@ -15,7 +15,6 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', isAuth,shopController.getCart);
 
-
 router.post('/cart', isAuth,shopController.postCart);
 
 router.post('/cart-delete-item', isAuth,shopController.postCartDeleteProduct);
@@ -23,5 +22,7 @@ router.post('/cart-delete-item', isAuth,shopController.postCartDeleteProduct);
 router.post('/create-order', isAuth,shopController.postOrder);
 
 router.get('/orders', isAuth,shopController.getOrders);
+
+router.get('/orders/:orderId', isAuth, shopController.getInvoice)
 
 module.exports = router;
