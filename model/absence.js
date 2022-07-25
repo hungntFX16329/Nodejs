d82f9794083765e67ffa597592a329a6233ad4db
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-25 16:00:16 +0700

    19 commit

--- model/absence.js
+++ model/absence.js
@@ -49,14 +49,14 @@ absenceSchema.statics.addAbsence = function (
     });
     return this.insertMany(newAbsence);
   } else if (type == 0) {
-    // const today = new Date(date);
-    // const yyyy = today.getFullYear();
-    // let mm = today.getMonth() + 1; // Months start at 0!
-    // let dd = today.getDate();
-    // let formattedToday = dd + '/' + mm + '/' + yyyy;
+    const today2 = new Date(date);
+    const yyyy2 = today2.getFullYear();
+    let mm2 = today2.getMonth() + 1; // Months start at 0!
+    let dd2 = today2.getDate();
+    let formattedToday2 = dd2 + '/' + mm2 + '/' + yyyy2;
     const newAbsence = {
       userId: userId,
-      date: new Date(date),
+      date: formattedToday2,
       days: hours / 8,
       reason: reason,
     };
