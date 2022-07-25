0a8e8b1d1504013c09bbb2d1bd27addb67bcefbc
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-25 15:52:52 +0700

    16 commits

--- model/absence.js
+++ model/absence.js
@@ -34,29 +34,29 @@ absenceSchema.statics.addAbsence = function (
     const dateArr = dates.split(",");
     const newAbsence = [];
     dateArr.forEach((date) => {
-      const today = new Date(date);
-      const yyyy = today.getFullYear();
-      let mm = today.getMonth() + 1; // Months start at 0!
-      let dd = today.getDate();
-      let formattedToday = dd + '/' + mm + '/' + yyyy;
+      // const today = new Date(date);
+      // const yyyy = today.getFullYear();
+      // let mm = today.getMonth() + 1; // Months start at 0!
+      // let dd = today.getDate();
+      // let formattedToday = dd + '/' + mm + '/' + yyyy;
 
       newAbsence.push({
         userId: userId,
-        date: formattedToday,
+        date: new Date(date),
         days: 1,
         reason: reason,
       });
     });
     return this.insertMany(newAbsence);
   } else if (type == 0) {
-    const today = new Date(date);
-    const yyyy = today.getFullYear();
-    let mm = today.getMonth() + 1; // Months start at 0!
-    let dd = today.getDate();
-    let formattedToday = dd + '/' + mm + '/' + yyyy;
+    // const today = new Date(date);
+    // const yyyy = today.getFullYear();
+    // let mm = today.getMonth() + 1; // Months start at 0!
+    // let dd = today.getDate();
+    // let formattedToday = dd + '/' + mm + '/' + yyyy;
     const newAbsence = {
       userId: userId,
-      date: formattedToday,
+      date: new Date(date),
       days: hours / 8,
       reason: reason,
     };
