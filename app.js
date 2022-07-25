cc0e2294af0c1b4c11077e7dcb89f061c5f6a82c
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-25 11:39:49 +0700

    3 commit

--- app.js
+++ app.js
@@ -110,10 +110,10 @@ mongoose
                 user.save();
             }
         })
-        .then((a) => {
-          app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
-            console.log("Connect with mongoBD");
-          });
-        });
+    .then((a) => {
+      app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+        console.log("Connect with mongoBD");
+      });
+    });
     })
     .catch(err=>console.log(err))
