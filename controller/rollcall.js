3c08b0e82fcea67fa37434efefd73ec9faf5bedd
Tien Hung <hungntFX16329@funix.edu.vn>
2022-07-25 15:38:09 +0700

    14 commit

--- controller/rollcall.js
+++ controller/rollcall.js
@@ -18,8 +18,8 @@ exports.getRollCallDetails = (req, res, next) => {
     let mm = today.getMonth() + 1; // Months start at 0!
     let dd = today.getDate();
 
-    if (dd < 10) dd = '0' + dd;
-    if (mm < 10) mm = '0' + mm;
+    // if (dd < 10) dd = '0' + dd;
+    // if (mm < 10) mm = '0' + mm;
 
     const formattedToday = dd + '/' + mm + '/' + yyyy;
     // let today = new Date().toLocaleDateString();
--- model/user.js
+++ model/user.js
@@ -51,8 +51,8 @@ userSchema.methods.getStatus = function(type, workplace) {
     let mm = today.getMonth() + 1; // Months start at 0!
     let dd = today.getDate();
 
-    if (dd < 10) dd = '0' + dd;
-    if (mm < 10) mm = '0' + mm;
+    // if (dd < 10) dd = '0' + dd;
+    // if (mm < 10) mm = '0' + mm;
 
     const formattedToday = dd + '/' + mm + '/' + yyyy;
     return Status.findOne({ userId: user._id })
